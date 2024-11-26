2fd7227d27ddcd88c882a8c4770e213ce8a5283d
Aaron Torres <aarontorres1423@gmail.com>
2024-11-26 18:41:57 -0500

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    'daisyui'
+  ],
+}
+
